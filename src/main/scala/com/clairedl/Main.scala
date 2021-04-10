9529db5ec4a832df1ba10bcc6845ea8e1ee3c4fa
ClaireDL <cl.delleluche@gmail.com>
2021-04-10 21:02:44 +0100

    Without buffer and global variable

--- src/main/scala/com/clairedl/Main.scala
+++ src/main/scala/com/clairedl/Main.scala
@@ -1,6 +1,5 @@
 package com.clairedl.scala
 
-import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 object Main extends App {
@@ -46,26 +45,26 @@ object Main extends App {
   case class Adult(name: String, drink: String, age: Int) extends Person
   case class Minor(name: String, drink: String, age: Int, parentName: String) extends Person
 
-  val file3 = Source
-    .fromFile("partycipants2.txt")
-    .getLines()
-    .toList
+  // val file3 = Source
+  //   .fromFile("partycipants2.txt")
+  //   .getLines()
+  //   .toList
 
-  var result = List[Person]()
+  // var result = List[Person]()
 
-  def isNumber(line: String): Boolean = line.matches("[0-9]+")
+  // def isNumber(line: String): Boolean = line.matches("[0-9]+")
 
-  for (line <- 2 to file3.length -1) {
-    if (isNumber(file3(line))) {
-      file3(line).toInt match {
-        case x if (x < 18)  =>
-          result = Minor(file3(line - 2), file3(line - 1), file3(line).toInt,file3(line + 1)) :: result
+  // for (line <- 2 to file3.length -1) {
+  //   if (isNumber(file3(line))) {
+  //     file3(line).toInt match {
+  //       case x if (x < 18)  =>
+  //         result = Minor(file3(line - 2), file3(line - 1), file3(line).toInt, file3(line + 1)) :: result
 
-        case x if (x >= 18) =>
-          result = Adult(file3(line - 2), file3(line - 1), file3(line).toInt) :: result
-      }
-    }
-  }
+  //       case x if (x >= 18) =>
+  //         result = Adult(file3(line - 2), file3(line - 1), file3(line).toInt) :: result
+  //     }
+  //   }
+  // }
 
   // result.map(x => println(x.name))
   // result.foreach(println)
@@ -74,31 +73,25 @@ object Main extends App {
   // Exercise 3: load one line at a time
   //
 
-  var result2 = List[Person]()
-  var buffer = List[String]()
-  var counter = 0
+  def process(fileName: String): List[Person] = {
+    val source = Source.fromFile(fileName).getLines().toList
+    var result = List[Person]()
 
-  for (line <- Source.fromFile("partycipants2.txt").getLines()) {
-    buffer = line :: buffer
-    counter += 1
+    for (line <- 2 to source.length - 1) {
+      source(line).toIntOption match {
+        case Some(x) => {
+          if (source(line).toInt < 18) result = Minor(source(line - 2), source(line - 1), source(line).toInt, source(line + 1)) :: result
 
-    if (counter == 4) {
-      if (buffer(1).toInt > 17) {
-        result2 = Adult(buffer(3), buffer(2), buffer(1).toInt) :: result2
-        val firstElement = buffer(0)
-        buffer = List()
-        buffer = firstElement :: buffer
-        counter = 1
-      }
-      else if (buffer(1).toInt < 18) {
-        result2 = Minor(buffer(3), buffer(2), buffer(1).toInt, buffer(0)) :: result2
-        buffer = List()
-        counter = 0
+          else if (source(line).toInt >= 18) result = Adult(source(line - 2), source(line - 1), source(line).toInt) :: result
+        }
+        case None =>
       }
     }
+    result
   }
 
+  val participants = process("partycipants2.txt")
+
   println(s"This is the final list")
-  result2.foreach(println)
-  result2.map(x => println(x.name))
+  participants.map(x => println(x))
 }
